dbbd8ed22b51aa635448db9618fb086be9d1d9bd
Mark Davison <markdavison0+github@gmail.com>
2021-10-24 13:01:55 +1300

    fix private route props

--- src/Components/PrivateRoute.tsx
+++ src/Components/PrivateRoute.tsx
@@ -4,6 +4,7 @@ import { useAuth } from './AuthContext';
 
 interface PrivateRouteProps {
     component: () => JSX.Element
+    path: string
 }
 
 const PrivateRoute = (props: PrivateRouteProps): JSX.Element => {
